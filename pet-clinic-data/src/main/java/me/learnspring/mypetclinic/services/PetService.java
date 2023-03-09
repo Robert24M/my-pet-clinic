307413c56b654e57d814ff391fe9c26806bc3e07
Robert <mirob1179@gmail.com>
2023-03-09 14:07:16 +0200

    refactor to common crud interface. closes #1

--- pet-clinic-data/src/main/java/me/learnspring/mypetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/me/learnspring/mypetclinic/services/PetService.java
@@ -2,11 +2,5 @@ package me.learnspring.mypetclinic.services;
 
 import me.learnspring.mypetclinic.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-    Pet findById(Long id);
-    Pet save(Pet pet);
-    Set<Pet> findAll();
-
+public interface PetService extends CrudService<Pet, Long> {
 }
