3d03a9fcaaa9147524d3b9d778c39b6b121ac5ba
Robert <mirob1179@gmail.com>
2023-03-16 22:54:29 +0200

    auto generated id on map impl. closes #22

--- pet-clinic-data/src/main/java/me/learnspring/mypetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/me/learnspring/mypetclinic/services/map/AbstractMapService.java
@@ -1,13 +1,15 @@
 package me.learnspring.mypetclinic.services.map;
 
+import me.learnspring.mypetclinic.model.BaseEntity;
+
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-public abstract class AbstractMapService<T, ID> {
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
 
-    protected Map<ID, T> map = new HashMap<>();
+    protected Map<Long, T> map = new HashMap<>();
 
     Set<T> findAll() {
        return new HashSet<>(map.values());
@@ -17,9 +19,16 @@ public abstract class AbstractMapService<T, ID> {
         return map.get(id);
     }
 
-    T save(ID id, T object) {
-        map.put(id, object);
+    T save(T object) {
 
+        if (object != null) {
+            if (object.getId() == null) {
+                object.setId(getNextId());
+            }
+            map.put(object.getId(), object);
+        }else {
+            throw new RuntimeException("Object can't be null");
+        }
         return object;
     }
 
@@ -31,4 +40,7 @@ public abstract class AbstractMapService<T, ID> {
         map.entrySet().removeIf(entry -> entry.getValue().equals(object));
     }
 
+    private Long getNextId() {
+        return (long) map.size() + 1;
+    }
 }
