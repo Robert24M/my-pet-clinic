cdbd3a58aa97dc0024386f40419d06c2972619f4
Robert <mirob1179@gmail.com>
2023-03-25 00:17:43 +0200

    added visits, updated owner with pet relationship. Closes #25

--- pet-clinic-data/src/main/java/me/learnspring/mypetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/me/learnspring/mypetclinic/model/Owner.java
@@ -1,5 +1,16 @@
 package me.learnspring.mypetclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person {
 
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
